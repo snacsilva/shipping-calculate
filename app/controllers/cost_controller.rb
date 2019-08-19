971e78059d7e21c257f46f8354b3b25e5a68615b
Samantha Silva <samanthasilva.n@gmail.com>
2019-08-19 17:30:55 -0300

    Rspec config.

--- app/controllers/cost_controller.rb
+++ app/controllers/cost_controller.rb
@@ -1,9 +1,23 @@
 class CostController < ApplicationController
 
   def index
+    # puts cookies[:origin]
+    p request.headers["Cookie"]
+
     distance = Distance.find_by(origin: cost_params[:origin], destination: cost_params[:destination])
-    @cost = distance.extension * cost_params[:weight] * 0.15    
-    render json: @cost
+    if distance
+      distance_or = Distance.where.not(id: distance.id).where("origin = ? or destination = ?", cost_params[:origin], cost_params[:destination])
+      total = distance_or.sum(:extension)
+
+      if distance.extension < total
+        @cost = distance.extension * cost_params[:weight] * 0.15    
+      else
+        @cost = total * cost_params[:weight] * 0.15    
+      end
+      render json: @cost
+    else
+      render json: { msg: "Caminho não existe no sistema."} , status: :unprocessable_entity
+    end
   end
 
   private
--- app/controllers/distances_controller.rb
+++ app/controllers/distances_controller.rb
@@ -1,15 +1,4 @@
 class DistancesController < ApplicationController
-  before_action :set_distance, only: [:show, :update, :destroy]
-
-  def index
-    @distances = Distance.all
-
-    render json: @distances
-  end
-
-  def show
-    render json: @distance
-  end
 
   def create
     @distance = Distance.find_by(origin: distance_params[:origin], destination: distance_params[:destination])
@@ -26,23 +15,7 @@ class DistancesController < ApplicationController
     end
   end
 
-  def update
-    if @distance.update(distance_params)
-      render json: @distance
-    else
-      render json: @distance.errors, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
-    @distance.destroy
-  end
-
   private
-    def set_distance
-      @distance = Distance.find(params[:id])
-    end
-
     def distance_params
       params.require(:distance).permit(:origin, :destination, :extension)
     end
